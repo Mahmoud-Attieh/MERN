04d525b1e542a18ecd4498daece17500634c266a
Mahmoud-Attieh <m-attieh1@outlook.com>
2023-08-27 09:29:14 +0300

    edit

--- Authors/server/server.js
+++ Authors/server/server.js
@@ -3,9 +3,9 @@ const cors = require('cors') // This is new
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
-require('./config/user.config'); // This is new
+require('./config/mongoose.config'); // This is new
 app.use(express.json()); // This is new
 app.use(express.urlencoded({ extended: true })); // This is new
 app.use(cors()) // This is new
-require('./routes/user.routes')(app);
+require('./routes/author.routes')(app);
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
